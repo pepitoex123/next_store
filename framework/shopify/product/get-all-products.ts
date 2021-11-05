d847a5dfcf988106c89735cb221b72f464c2d14b
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-11-05 14:11:43 -0300

    refactored fetch-api.ts to include better typescript implementation

--- framework/shopify/product/get-all-products.ts
+++ framework/shopify/product/get-all-products.ts
@@ -1,12 +1,21 @@
 import getAllProductsQuery from "../utils/queries/get-all-products";
 import fetchApi from "../utils/fetch-api";
+import {ProductConnection} from "../schema";
+
+type ReturnType = {
+    products: ProductConnection
+}
+
+
+const getAllProducts = async (): Promise<any> => {
+    const {data} = await fetchApi<ReturnType>({query: getAllProductsQuery});
+
+    // Normalize and return new data
 
 
 
 
-const getAllProducts = async (): Promise<any[]> => {
-    const products = await fetchApi({query: getAllProductsQuery});
-    return products.data
+    return data.products
 }
 
 
