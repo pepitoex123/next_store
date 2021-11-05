261eb258ed3e9b3fe685b9cdad501e261425afae
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-11-05 15:37:28 -0300

    added withFramework settings for next.config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
-module.exports = {
+
+const {withFrameworkConfig} = require("./framework/common/config")
+
+
+module.exports = withFrameworkConfig({
   reactStrictMode: true,
   i18n: {
     locales: ["en-US","es"],
     defaultLocale: "en-US"
   }
-}
+})
 
 
 console.log("next.config.js", JSON.stringify(module.exports,null,2))
