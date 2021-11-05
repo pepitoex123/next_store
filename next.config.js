a1bf72455f10ec1ee167000501942ace0e7c626a
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-11-05 15:26:09 -0300

    created configuration files for bigcommerce,shopify and general project

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   reactStrictMode: true,
+  i18n: {
+    locales: ["en-US","es"],
+    defaultLocale: "en-US"
+  }
 }
+
+
+console.log("next.config.js", JSON.stringify(module.exports,null,2))
