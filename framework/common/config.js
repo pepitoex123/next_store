ce9494ab880df9726442ded2c95ae8f950b6cd9d
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-11-05 15:51:48 -0300

    added automatic config modification for framework

--- framework/common/config.js
+++ framework/common/config.js
@@ -1,14 +1,24 @@
 
 
-const path = require("path")
-const merge = require("deepmerge")
+const path = require("path");
+const fs = require("fs");
+const merge = require("deepmerge");
 
 function withFrameworkConfig(defaultConfig = {}) {
     const framework = "shopify"
 
     const frameworkNextConfig = require(path.join("../",framework,"next.config"))
-
     const config = merge(defaultConfig,frameworkNextConfig)
+
+    const tsConfig = require(path.join(process.cwd(),"tsconfig.json"))
+
+    tsConfig.compilerOptions.paths["@framework"] = [`framework/${framework}`]
+    tsConfig.compilerOptions.paths["@framework/*"] = [`framework/${framework}/*`]
+
+    fs.writeFileSync(
+        path.join(process.cwd(),"tsconfig.json"),
+        JSON.stringify(tsConfig,null,2)
+    )
 
     return config
 }
