14a7fd1a5f05c8e7b9c5edd06614ab25a5a262fc
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-11-07 19:18:49 -0300

    started developing ProductCard component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,15 @@ import type {InferGetStaticPropsType} from "next";
 import getAllProducts from "@framework/product/get-all-products";
 import {getConfig} from "@framework/api/config";
 import {Layout} from "@components/common";
+import {ProductCard} from "@components/Product";
 
 export default function Home({products}: InferGetStaticPropsType<typeof getStaticProps>) {
 
     return (
         <div>
-                {JSON.stringify(products)}
+            {
+                products.slice(0,3).map(product => <ProductCard key={product.id} product={product}/>)
+            }
         </div>
   )
 }
