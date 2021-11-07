84a1baba997f40e982e51b6bfdc9740ec807c6ab
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-11-07 15:41:30 -0300

    added css variables in base.css

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -5,7 +5,7 @@ import styles from "./Layout.module.css";
 const Layout: FC = ({children}) => {
     return(
         <div className={styles.root}>
-            <main className="fit">
+            <main style={{color: "var(--primary)"}} className="fit">
                 {children}
             </main>
         </div>
