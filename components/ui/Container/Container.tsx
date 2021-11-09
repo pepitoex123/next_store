e0b7a254b0ebebbf5cc9240c8c7a06305bf5f7a9
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-11-09 18:25:27 -0300

    added more customization to Container component

--- components/ui/Container/Container.tsx
+++ components/ui/Container/Container.tsx
@@ -3,13 +3,14 @@ import {FC, ReactNode} from "react";
 
 interface Props {
     children: ReactNode | ReactNode[]
+    el?: any
 }
 
-const Container: FC<Props> = ({children}) => {
+const Container: FC<Props> = ({children,el: Component="div"}) => {
     return(
-        <div style={{maxWidth: "1920px"}} className="px-6 mx-auto">
+        <Component className="px-6 mx-auto max-w-8xl">
             {children}
-        </div>
+        </Component>
     )
 }
 
--- components/ui/Hero/Hero.tsx
+++ components/ui/Hero/Hero.tsx
@@ -12,7 +12,7 @@ interface Props {
 const Hero: FC<Props> = ({headline,description}) => {
     return(
         <div className="bg-black">
-            <Container>
+            <Container el="div">
                 <div className={styles.root}>
                     <h2 className={styles.headline}>
                         {headline}
