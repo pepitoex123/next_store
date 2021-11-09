9b1f46dacd86fae44a647e6818184237ec2ac022
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-11-09 14:39:53 -0300

    added classnames module, started developing layout A and layout B

--- components/ui/Grid/Grid.tsx
+++ components/ui/Grid/Grid.tsx
@@ -1,11 +1,23 @@
 
 import {FC,ReactNode} from "react";
 import styles from "./Grid.module.css";
+import cn from "classnames";
+
+interface Props {
+    children: ReactNode[],
+    layout?: "A" | "B"
+}
 
 
-const Grid: FC<ReactNode> = ({children}) => {
+const Grid: FC<Props> = ({children,layout = "A"}) => {
+
+    const rootClassName = cn(styles.root, {
+        [styles.layoutA]: layout === "A",
+        [styles.layoutB]: layout === "B"
+    })
+
     return(
-        <div className={styles.root}>
+        <div className={rootClassName}>
             {children}
         </div>
     )
