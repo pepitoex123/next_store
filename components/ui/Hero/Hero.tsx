4799bb741c8445511299a14bbab10e35a41c41c8
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-11-09 18:15:06 -0300

    added Container component, modified Hero component to implement Container component

--- components/ui/Hero/Hero.tsx
+++ components/ui/Hero/Hero.tsx
@@ -1,6 +1,7 @@
 import {FC} from "react";
 import styles from "./Hero.module.css";
 import Link from "next/link";
+import {Container} from "@components/ui";
 
 interface Props {
     headline: string
@@ -11,21 +12,23 @@ interface Props {
 const Hero: FC<Props> = ({headline,description}) => {
     return(
         <div className="bg-black">
-            <div className={styles.root}>
-                <h2 className={styles.headline}>
-                    {headline}
-                </h2>
-                <div>
-                    <p className={styles.description}>
-                        {description}
-                    </p>
-                    <Link href="/">
-                        <a className={styles.link}>
-                            Read it here
-                        </a>
-                    </Link>
+            <Container>
+                <div className={styles.root}>
+                    <h2 className={styles.headline}>
+                        {headline}
+                    </h2>
+                    <div>
+                        <p className={styles.description}>
+                            {description}
+                        </p>
+                        <Link href="/">
+                            <a className={styles.link}>
+                                Read it here
+                            </a>
+                        </Link>
+                    </div>
                 </div>
-            </div>
+            </Container>
         </div>
     )
 }
