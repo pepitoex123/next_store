9da65f7949df1487b6ef40ec9c6a6aa5fbe9e4de
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-11-10 12:08:21 -0300

    added styling for layout B in Grid Component

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,6 +22,11 @@ export default function Home({products}: InferGetStaticPropsType<typeof getStati
                     products.slice(0,3).map(product => <ProductCard key={product.id} product={product} variant="slim"/>)
                 }
             </Marquee>
+            <Grid layout="B">
+                {
+                    products.slice(0,3).map(product => <ProductCard key={product.id} product={product} variant="simple"/>)
+                }
+            </Grid>
         </>
   )
 }
