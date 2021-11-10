76644fb6372246668d94862d6f04fda30292e6bf
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-11-10 12:42:06 -0300

    navbar implementation finished

--- components/common/Navbar/Navbar.tsx
+++ components/common/Navbar/Navbar.tsx
@@ -1,11 +1,33 @@
 import {FC} from "react";
 import {Container} from "@components/ui";
+import Link from "next/link";
+import styles from "./Navbar.module.css"
 
 const Navbar: FC = () => {
     return(
         <Container>
-            <div className="flex flex-row md:py-6">
-                <h1>URI_STORE</h1>
+            <div className={styles.root}>
+                <div className="flex flex-1 items-center">
+                    <Link href="/">
+                        <a className={styles.logo}>
+                            URI_STORE
+                        </a>
+                    </Link>
+                    <nav className="ml-6 space-x-6">
+                        <Link href="/">
+                            <a className={styles.link}>All</a>
+                        </Link>
+                        <Link href="/">
+                            <a className={styles.link}>Clothes</a>
+                        </Link>
+                        <Link href="/">
+                            <a className={styles.link}>Accesories</a>
+                        </Link>
+                        <Link href="/">
+                            <a className={styles.link}>Shoes</a>
+                        </Link>
+                    </nav>
+                </div>
             </div>
         </Container>
     )
