54a29ce37cf6a101d6bb774b120b6b774ded6933
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-11-10 13:02:16 -0300

    added icons that will be used during the project, implemented Cart and Heart icons

--- components/common/Usernav/Usernav.tsx
+++ components/common/Usernav/Usernav.tsx
@@ -1,19 +1,19 @@
 import {FC} from "react";
 import styles from "./Usernav.module.css";
 import Link from "next/link";
-
+import {Bag as Cart,Heart} from "@components/icons";
 
 const Usernav: FC = () => {
     return(
         <nav>
             <ul className={styles.list}>
                 <li className={styles.item}>
-                    Cart
+                    <Cart/>
                 </li>
                 <li className={styles.item}>
                     <Link href="/">
                         <a>
-                            Wishlist
+                            <Heart/>
                         </a>
                     </Link>
                 </li>
