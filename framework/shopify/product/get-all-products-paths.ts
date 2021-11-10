97e811b704eaf7b249eb456ff3e0f4eea356e82c
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-11-10 17:37:05 -0300

    added dynamic getStaticPaths functionality

--- framework/shopify/product/get-all-products-paths.ts
+++ framework/shopify/product/get-all-products-paths.ts
@@ -1,6 +1,7 @@
 import {ApiConfig} from "@common/types/api";
 import {ProductConnection} from "@framework/schema";
 import {Product} from "@common/types/product";
+import getAllProductsPathsQuery from "@framework/utils/queries/get-all-products-paths";
 
 
 type ReturnType = {
@@ -8,12 +9,22 @@ type ReturnType = {
 }
 
 const getAllProductsPaths = async(config: ApiConfig): Promise<ReturnType> => {
+
+    const {data} = await config.fetch<{products: ProductConnection}>({
+        query: getAllProductsPathsQuery,
+        url: config.apiUrl
+    })
+
+    const products = data.products.edges.map(({node: {handle}}) => {
+        return {
+            slug: handle
+        }
+    })
+
+    console.log(products)
+
     return {
-        products: [
-            {slug: "cool-hat"},
-            {slug: "t-shirt"},
-            {slug: "lightweight-jacket"}
-        ]
+        products
     }
 }
 
