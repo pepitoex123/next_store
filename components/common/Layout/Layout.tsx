649e78d2e6c4509aaa8bc361c94f2f61cdb07be4
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-11-10 12:31:56 -0300

    basic navbar implementation

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -2,10 +2,12 @@
 import {FC} from "react";
 import styles from "./Layout.module.css";
 import {Footer} from "@components/common";
+import {Navbar} from "@components/common";
 
 const Layout: FC = ({children}) => {
     return(
         <div className={styles.root}>
+            <Navbar/>
             <main className="fit">
                 {children}
             </main>
