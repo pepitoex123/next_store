b9d1d93dc9c0bc1b0f3ff3b3d557eeea978751ab
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-11-12 18:54:25 -0300

    started implementing slider functionality

--- components/Product/ProductSlider/ProductSlider.tsx
+++ components/Product/ProductSlider/ProductSlider.tsx
@@ -1,12 +1,31 @@
-import {FC} from "react";
+import React,{FC, Children, isValidElement} from "react";
 import styles from "./ProductSlider.module.css";
 
 
 const ProductSlider: FC = ({children}) => {
     return(
         <div className={styles.root}>
-            <div className="h-full transition-opacity">
-                {children}
+            <div className="keen-slider h-full transition-opacity">
+                {
+                    Children.map(children, (child) => {
+                        if(isValidElement(child)){
+
+                            return {
+                                ...child,
+                                props: {
+                                    ...child.props,
+                                    className: "keen-slider__slide"
+                                }
+                            }
+
+
+                            // return React.cloneElement(child, {className: "keen-slider__slide"})
+                        }
+
+                        return child
+                    }
+                    )
+                }
             </div>
         </div>
     )
